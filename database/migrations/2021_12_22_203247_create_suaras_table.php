9e33244783e550dc5dd6594eb595bebfc50bfd40
yogafebriatala24 <yogafebriatala24@gmail.com>
2021-12-23 03:38:35 +0700

    migration suara

---
+++ database/migrations/2021_12_22_203247_create_suaras_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuarasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suaras', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('paslon_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suaras');
+    }
+}
