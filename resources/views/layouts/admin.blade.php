c08d24224391a6dbc0ad3f8fbad0bea3d8e67487
gusti126 <gustidoang936@gmail.com>
2021-12-23 16:28:43 +0700

    change sidebar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -25,13 +25,7 @@
 
         <div class="pagetitle">
             <h1>@yield('title', 'Dashboard')</h1>
-            <nav>
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                    <li class="breadcrumb-item">Pages</li>
-                    <li class="breadcrumb-item active">Blank</li>
-                </ol>
-            </nav>
+
         </div><!-- End Page Title -->
 
         <section class="section">
