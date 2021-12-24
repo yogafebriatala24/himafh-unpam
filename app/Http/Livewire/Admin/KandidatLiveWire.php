c990bfa86f3c68821ad20b213a473e634d6c2345
gusti126 <gustidoang936@gmail.com>
2021-12-24 16:40:32 +0700

    add show list paslon

--- app/Http/Livewire/Admin/KandidatLiveWire.php
+++ app/Http/Livewire/Admin/KandidatLiveWire.php
@@ -3,14 +3,21 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\Kandidat;
+use App\Models\Paslon;
 use Livewire\Component;
 
 class KandidatLiveWire extends Component
 {
     public function render()
     {
+        $paslon = Paslon::with('kandidat.user')->get();
+        // dd($paslon);
         $kandidats = Kandidat::get();
-        return view('livewire.admin.kandidat-live-wire')
+
+        return view('livewire.admin.kandidat-live-wire', [
+            'kandidats' => $kandidats,
+            'paslons' => $paslon
+        ])
             ->extends('layouts.admin')->section('content');
     }
 }
