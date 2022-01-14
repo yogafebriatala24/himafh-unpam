101350a2eded1e19d923d3ba068aa673e64bb6b0
yogafebriatala24 <yogafebriatala24@gmail.com>
2022-01-15 03:03:49 +0700

    change seeder

--- resources/views/livewire/front/voting-live-wire.blade.php
+++ resources/views/livewire/front/voting-live-wire.blade.php
@@ -13,27 +13,31 @@
     <div class="row justify-content-center">
         @foreach ($paslons as $item)
         <div class="col-12 col-lg-4 d-flex align-items-stretch mt-2">
-            <div class="member" data-aos="fade-up" data-aos-delay="100">
-                <img src="{{ asset('/storage/' . $item->foto) }}"
-                                    class="img-fluid" alt="" />
-                <div class="row text-center">
-                    <div class="col">
-                        <div class="member-info">
-                            <h6 class="mt-2" style="font-weight: bold">{{ $item->ketua->user->name }}</h6>
-                            <span style="font-size: 11px">Calon Ketua Umum</span>
+            <div class="card">
+                <div class="card-body">
+                    <div class="member" data-aos="fade-up" data-aos-delay="100">
+                        <img src="{{ asset('/storage/' . $item->foto) }}"
+                                            class="img-fluid" alt="" />
+                        <div class="row text-center">
+                            <div class="col">
+                                <div class="member-info">
+                                    <h6 class="mt-2" style="font-weight: bold">{{ $item->ketua->user->name }}</h6>
+                                    <span style="font-size: 11px">Calon Ketua Umum</span>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="member-info">
+                                    <h6 class="mt-2" style="font-weight: bold">{{ $item->wakil->user->name }}</h6>
+                                    <span style="font-size: 11px">Calon Wakil Ketua Umum</span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col">
-                        <div class="member-info">
-                            <h6 class="mt-2" style="font-weight: bold">{{ $item->wakil->user->name }}</h6>
-                            <span style="font-size: 11px">Calon Wakil Ketua Umum</span>
+                        <div class="d-grid gap-2 mt-2">
+                            <a href="{{route('vote', $item->id)}}" class="btn btn-danger btn-sm text-center">Vote Pasangan Calon 0{{ $item->nomor }}</a>
                         </div>
+        
                     </div>
                 </div>
-                <div class="d-grid gap-2 mt-2">
-                    <a href="{{route('vote', $item->id)}}" class="btn btn-danger btn-sm text-center">Vote Pasangan Calon 0{{ $item->nomor }}</a>
-                </div>
-
             </div>
         </div>
         {{-- <div class="col-md-6">
