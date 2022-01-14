77664cff49ee7ed56abf0c59fe444aa24cdd80ab
yogafebriatala24 <yogafebriatala24@gmail.com>
2022-01-15 01:11:48 +0700

    add new meta tag

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,10 @@
     <meta property="og:title" content="Himpunan Mahasiswa Fakultas Hukum Universitas Pamulang 2022" />
     <meta property="og:description" content="Himpunan Mahasiswa Fakultas Hukum Universitas Pamulang 2022" />
     <Meta Content='Himpunan Mahasiswa Hukum Universitas Pamulang' Name='description'/>
+    <meta property="og:image:type" content="image">
+    <!-- Size of image. Any size up to 300. Anything above 300px will not work in WhatsApp -->
+    <meta property="og:image:width" content="300">
+    <meta property="og:image:height" content="300">
     <meta property="og:type" content="website" />
     <meta property="og:image" content="https://himafhunpam.com/images/logo.png">
     <meta property="og:image" itemprop="image" content="https://himafhunpam.com/images/logo.png">
