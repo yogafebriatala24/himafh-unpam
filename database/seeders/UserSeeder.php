846cd6744b3d80748e2b68485dc81b8638c09307
yogafebriatala24 <yogafebriatala24@gmail.com>
2022-01-15 02:22:32 +0700

    change seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,69 +17,17 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        // 'name',
-        // 'email',
-        // 'password',
-        // 'username', 'no_tlp',
-        // 'kelas', 'reguler', 'roles'
-        // $faker = Faker::create('id_ID');
         User::create([
             'name' => 'Admin Hima FH',
             'email' => 'email@istimewah.com',
-            'password' => Hash::make('admin2022'),
-            'passwordtwo' => Crypt::encryptString('admin2022'),
-            'username' => 1,
+            'password' => Hash::make('111'),
+            'passwordtwo' => Crypt::encryptString('111'),
+            'username' => 123,
             'no_tlp' => '001',
             'kelas' => '001',
             'reguler' => 'Z',
             'roles' => 'admin'
         ]);
-        // User::create([
-        //     'name' => 'Admin Dua Hima FH',
-        //     'email' => 'email@istimewah2.com',
-        //     'username' => 1,
-        //     'no_tlp' => '001',
-        //     'kelas' => '001',
-        //     'reguler' => 'Z',
-        //     'roles' => 'admin'
-        // ]);
-        // for ($i = 0; $i < 10; $i++) {
-        //     User::create([
-        //         'name' => $faker->name,
-        //         'email' => $faker->email,
-        //         'password' => Hash::make('admin2022'),
-        //         'passwordtwo' => Crypt::encryptString('admin2022'),
-        //         'username' => rand(10000000, 100000000),
-        //         'no_tlp' => $faker->phoneNumber,
-        //         'kelas' => 'TPLA' . $i,
-        //         'reguler' => 'A',
-        //     ]);
-        // }
-        // for ($i = 0; $i < 10; $i++) {
-        //     User::create([
-        //         'name' => $faker->name,
-        //         'email' => $faker->email,
-        //         'password' => Hash::make('admin2022'),
-        //         'passwordtwo' => Crypt::encryptString('admin2022'),
-        //         'username' =>
-        //         rand(10000000, 100000000),
-        //         'no_tlp' => $faker->phoneNumber,
-        //         'kelas' => 'TPLB' . $i,
-        //         'reguler' => 'B',
-        //     ]);
-        // }
-        // for ($i = 0; $i < 3; $i++) {
-        //     User::create([
-        //         'name' => $faker->name,
-        //         'email' => $faker->email,
-        //         'password' => Hash::make('admin2022'),
-        //         'passwordtwo' => Crypt::encryptString('admin2022'),
-        //         'username' =>
-        //         rand(10000000, 100000000),
-        //         'no_tlp' => $faker->phoneNumber,
-        //         'kelas' => 'TPLC' . $i,
-        //         'reguler' => 'C',
-        //     ]);
-        // }
+        
     }
 }
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.front')
+
+@section('title')
+    Error Page
+@endsection
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <img src="{{asset('images/505.jpg')}}" class="img-fluid" alt="">
+        </div>
+        <div class="col-8 text-center">
+            <a href="/" class="btn btn-danger btn-block align-items-center">Back To Home</a>
+        </div>
+    </div>
+@endsection
