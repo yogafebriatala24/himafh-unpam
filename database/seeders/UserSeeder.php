29fb13f0f0e5c93f8817b8f6197c7a91786da59c
yogafebriatala24 <yogafebriatala24@gmail.com>
2022-01-16 14:51:37 +0700

    change seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,10 +19,10 @@ class UserSeeder extends Seeder
     {
         User::create([
             'name' => 'Admin Hima FH 3',
-            'email' => 'lawtech32022@gmail.com',
-            'password' => Hash::make('*Adminnnn3.20222023*'),
-            'passwordtwo' => Crypt::encryptString('*Adminnnn3.20222023*'),
-            'username' => 'adminpemira32022',
+            'email' => 'lawtech2022@gmail.com',
+            'password' => Hash::make('*Adminnnn.20222023*'),
+            'passwordtwo' => Crypt::encryptString('*Adminnnn.20222023*'),
+            'username' => 'adminpemira2022',
             'no_tlp' => '001',
             'kelas' => '001',
             'reguler' => 'Z',
